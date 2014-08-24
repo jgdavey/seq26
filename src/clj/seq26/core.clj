2f2c995a8a89f075dd71f2235bc4f703f153e247
Joshua Davey <josh@joshuadavey.com>
2014-08-23 19:48:30 -0500

    Proxy to middleman

--- src/clj/seq26/core.clj
+++ src/clj/seq26/core.clj
@@ -1,15 +1,52 @@
 (ns seq26.core
-    (:require [compojure.handler :as handler]
-              [compojure.route :as route]
-              [compojure.core :refer [GET POST defroutes]]
-              [ring.util.response :as resp]
-              [clojure.java.io :as io]))
+  (:require [compojure.route :as route]
+            [compojure.core :refer [GET POST defroutes]]
+            [compojure.response :as response]
+            [ring.util.mime-type :refer [ext-mime-type]]
+            [ring.util.response :as r]
+            [ring.middleware.head :refer [wrap-head]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.nested-params :refer [wrap-nested-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]])
+  (:import [java.net URL]))
+
+(defn- request-path
+  "Return the path + query of the request."
+  [request]
+  (str (:uri request)
+       (if-let [query (:query-string request)]
+         (str "?" query))))
+
+(defn- content-type-response
+  [resp req & [opts]]
+  (if (r/get-header resp "Content-Type")
+    resp
+    (if-let [mime-type (ext-mime-type (:uri req) (:mime-types opts))]
+      (r/content-type resp mime-type)
+      resp)))
+
+(defn wrap-content-type [handler & [opts]]
+  (fn [req]
+    (if-let [resp (handler req)]
+      (content-type-response resp req opts))))
+
+(defn proxy-response
+  "A route that returns a 404 not found response, with its argument as the
+  response body."
+  [url-root]
+  (->
+    (fn [request]
+      (let [url (URL. (str url-root (request-path request)))]
+        (response/render url request)))
+    wrap-content-type
+    wrap-head))
 
 (defroutes app-routes
-  (GET "/" [] (resp/redirect "/index.html"))
   (route/resources "/")
-  (route/not-found "Page not found"))
+  (proxy-response "http://localhost:4567"))
 
 (def app
   (-> #'app-routes
-      (handler/api)))
+      wrap-keyword-params
+      wrap-nested-params
+      wrap-params))
