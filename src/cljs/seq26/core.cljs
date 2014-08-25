47b1caddaffdc0bee464c8feb4ef743dfcf12804
Joshua Davey <josh@joshuadavey.com>
2014-08-24 22:08:41 -0500

    Basic rendering from app state

--- src/cljs/seq26/core.cljs
+++ src/cljs/seq26/core.cljs
@@ -12,17 +12,52 @@
 (defprotocol Instrument
   (-play-at [this midi on-at off-at]))
 
+(def note-names [:C :Db :D :Eb :E :F :Gb :G :Ab :A :Bb :B])
+
+(def keyboard-notes
+  (let [start-octave 3
+        start-midi (* (inc start-octave) 12)
+        notes (for [oct (range start-octave 6)
+                    note (map name note-names)]
+                [oct note])]
+    (mapv (fn [[oct note] i] {:label (str (name note) oct)
+                              :midi i
+                              :note note
+                              :octave oct
+                              :color (if (= 2 (count (name note))) "black" "white")}
+            ) notes (iterate inc start-midi))))
+
 (def app-state
-  (atom {:params {:bpm 120}}))
+  (atom {:params {:bpm 120}
+         :playback {:zero-t 0.0
+                    :playing? false}
+         :sequence [{:midi 69 :beat 0 :length 1}
+                    {:midi 73 :beat 0.25 :length 1}
+                    {:midi 76 :beat 0.5 :length 1}
+                    {:midi 52 :beat 1 :length 0.5}
 
-(defn seq26-app [app owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div nil
-        (dom/h1 nil "seq26 is working!")))))
+                    {:midi 69 :beat 2 :length 1}
+                    {:midi 73 :beat 2 :length 1}
+                    {:midi 76 :beat 2 :length 1}
+                    {:midi 52 :beat 3 :length 0.5}
 
-(om/root seq26-app app-state {:target (.getElementById js/document "content")})
+                    {:midi 69 :beat 4 :length 1}
+                    {:midi 74 :beat 4.25 :length 1}
+                    {:midi 76 :beat 4.5 :length 1}
+                    {:midi 52 :beat 5 :length 0.5}
+
+                    {:midi 69 :beat 6 :length 1}
+                    {:midi 74 :beat 6 :length 1}
+                    {:midi 76 :beat 6 :length 1}
+                    {:midi 52 :beat 7 :length 0.5} ] }))
+
+(defn- assoc-notes [keys notes]
+  (map (fn [k]
+         (assoc k :notes (filterv (fn [n]
+                                    (= (:midi k) (:midi n))) notes)))
+       keys))
+
+;; Audio
 
 (def ctx (hum/create-context))
 (def output (hum/create-gain ctx))
@@ -103,20 +138,19 @@
 (def dejitter-factor (beat->s 1)) ; one beat of dejitter
 
 (defn current-beat [zero]
-  (s->beats (+ (- (.-currentTime ctx) zero) dejitter-factor)))
+  (s->beats (- (.-currentTime ctx) zero)))
 
 (defn scheduler
   "Takes an 'instrument'.
   Returns a core/async channel; each message to this channel must be
   a note, and will be scheduled with -play-at
   Notes should be scheduled in order."
-  [inst]
-  (let [zero-t (+ (.-currentTime ctx) dejitter-factor) ; avoid jitter
-        c (chan 8)]
+  [zero-t inst]
+  (let [c (chan 8)]
     (go (loop []
           (when-let [n (<! c)]
             (let [beat (current-beat zero-t)]
-              (when (> (- (:beat n) beat) 0) ; when more than a beat ahead (jitter applied)
+              (when (> (- (:beat n) beat) 1) ; when more than a beat ahead (jitter applied)
                 (<! (timeout (* 1000 (beat->s 1))))) ; wait for one beat, then proceed
               (let [{:keys [midi beat length]} n
                     from (+ zero-t (beat->s beat))
@@ -130,33 +164,127 @@
    (play notes oscillator))
   ([notes inst-fn]
    (let [inst (make-inst inst-fn (set (map :midi notes)))
-         sch (scheduler inst)]
-     (async/onto-chan sch notes))))
+         zero-t (+ (.-currentTime ctx) dejitter-factor)
+         sch (scheduler zero-t inst)]
+     (async/onto-chan sch notes)
+     zero-t)))
 
+(defn play! []
+  (let [zero-t (play (:sequence @app-state))
+        cb (fn cb []
+             (when (get-in @app-state [:playback :playing?])
+               (swap! app-state assoc-in [:playback :current-beat] (current-beat zero-t))
+               (js/requestAnimationFrame cb)))]
+     (swap! app-state update-in [:playback] assoc :zero-t zero-t, :playing? true)
+     (js/requestAnimationFrame cb)))
+
+(defn stop! []
+  (swap! app-state assoc-in [:playback :playing?] false))
+
+;; Om components
+
+(defn piano-key [{:keys [octave label color note]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li #js { :className (str color " note-" note)}
+              label))))
+
+(defn note-view [{:keys [beat length selected?] :as note} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className (if selected? "selected on" "on")
+                    :onClick (fn [e]
+                               (.preventDefault e)
+                               (om/update! note [:selected?] (not selected?)))
+                    :style #js {:left  (str (/ beat 0.16) "%")
+                                :width (str (/ length 0.16) "%")}}
+               ""))))
+
+(defn lane-view [{:keys [octave label color note notes]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/div #js {:className (str color " lane")}
+             (apply dom/div #js {:className "notes"}
+                    (om/build-all note-view notes))
+             (map (fn [i]
+                    (dom/div #js {:className "beat-marker"
+                                  :style #js {:left (str (/ i 0.16) "%")}}))
+                  (range 17))))))
+
+(defn- marker-percent [zero-t]
+  (let [b (current-beat zero-t)]
+    (if (neg? b)
+      0.0
+      (/ (mod b 16) 0.16))))
+
+(defn playhead-view [{:keys [zero-t playing?]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (when playing?
+        (dom/div #js {:className "playhead"
+                      :style #js {:left (str (marker-percent zero-t) "%")}})))))
+
+(defn seq26-app [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+        (dom/h1 nil "seq26 is working!")
+        (dom/div #js {:id "app"}
+          (dom/div #js {:id "pianoroll"}
+            (dom/div #js {:id "keys"}
+              (apply dom/ul #js {}
+                (om/build-all piano-key (reverse keyboard-notes))))
+            (apply dom/div #js {:id "lanes"}
+                   (om/build playhead-view (:playback app))
+                   (om/build-all lane-view
+                                 (assoc-notes (reverse keyboard-notes) (:sequence app))))))))))
+
+(om/root seq26-app app-state {:target (.getElementById js/document "content")})
+
+(defn- transform [pred transform-fn item]
+  (if (pred item)
+    (transform-fn item)
+    item))
+
+(defn- f-attr [f attr]
+  (fn [item]
+    (update-in item [attr] f)))
+
+(let [up (f-attr inc :midi)
+      down (f-attr dec :midi)
+      right (f-attr (partial + 0.25) :beat)
+      left (f-attr #(- % 0.25) :beat)
+      update! (fn [f]
+                (swap! app-state update-in [:sequence]
+                       #(mapv (partial transform :selected? f) %)))]
+
+  (def keyboard-events
+    {32 (fn [e]
+          (if (get-in @app-state [:playback :playing?])
+            (stop!)
+            (play!)))
+     37 (fn [e] (update! left))
+     38 (fn [e] (update! up))
+     39 (fn [e] (update! right))
+     40 (fn [e] (update! down))}))
+
+(let [c (chan 1)]
+  (go (loop []
+        (when-let [event (<! c)]
+          (when-let [f (keyboard-events (.-keyCode event))]
+            (.preventDefault event)
+            (f event))
+          (recur))))
+  (.addEventListener js/document "keydown" (fn [e] (put! c e))))
 
 (comment
 
-(play [{:midi 69 :beat 0 :length 1}
-       {:midi 73 :beat 0.25 :length 1}
-       {:midi 76 :beat 0.5 :length 1}
-       {:midi 52 :beat 1 :length 0.5}
-
-       {:midi 69 :beat 2 :length 1}
-       {:midi 73 :beat 2 :length 1}
-       {:midi 76 :beat 2 :length 1}
-       {:midi 52 :beat 3 :length 0.5}
-
-       {:midi 69 :beat 4 :length 1}
-       {:midi 74 :beat 4.25 :length 1}
-       {:midi 76 :beat 4.5 :length 1}
-       {:midi 52 :beat 5 :length 0.5}
-
-       {:midi 69 :beat 6 :length 1}
-       {:midi 74 :beat 6 :length 1}
-       {:midi 76 :beat 6 :length 1}
-       {:midi 52 :beat 7 :length 0.5} ]
-      bell)
-
+(deref app-state)
 (swap! app-state assoc-in [:params :bpm] 100)
 
 )
